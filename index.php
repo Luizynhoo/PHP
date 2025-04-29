0d8335b545b828ee141fa3e3dd4edd8ad6030a12
Luiz Costa <lhcosta@R11Travel.local>
2025-04-29 11:26:22 -0300

    Php update

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head></head>
+<body>
+    <!-- echo = console.log -->
+    <?php
+    <!--$ para criar variavel-->
+    $adjt = "Lindo";
+    echo "<p>Olá, ". $adjt ." Mundo!</p>"
+    ?>
+</body>
+</html>
